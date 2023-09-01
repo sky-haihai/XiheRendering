f38c59294d90c8332c8ab8088ee3e974cac97493
sky <skyhaihai2000@gmail.com>
2023-09-01 17:34:26 -0600

    add osp

--- ObjectSpacePixelation/ObjectSpacePixelationFeature.cs
+++ ObjectSpacePixelation/ObjectSpacePixelationFeature.cs
@@ -1,29 +1,46 @@
 using System;
 using UnityEngine;
 using UnityEngine.Rendering.Universal;
+using UnityEngine.Serialization;
 
 namespace XihePostProcessing.ObjectSpacePixelation {
     public class ObjectSpacePixelationFeature : ScriptableRendererFeature {
         [SerializeField]
         public Settings settings = new Settings();
 
-        ObjectSpacePixelationPass m_ObjectSpacePixelationPass;
+        PixelationMapPass m_PixelationMapPass;
+        UsePixelationMapPass m_UsePixelationMapPass;
 
         public override void Create() {
-            m_ObjectSpacePixelationPass = new ObjectSpacePixelationPass(settings) {
+            m_PixelationMapPass = new PixelationMapPass(settings) {
+                renderPassEvent = settings.renderPassEvent
+            };
+
+            m_UsePixelationMapPass = new UsePixelationMapPass(settings) {
                 renderPassEvent = settings.renderPassEvent
             };
         }
 
         public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData) {
-            if (settings.computeShader == null) return;
-            
-            renderer.EnqueuePass(m_ObjectSpacePixelationPass);
+            if (settings.drawPixelationMapShader == null) return;
+            renderer.EnqueuePass(m_PixelationMapPass);
+
+            if (settings.usePixelationShader == null) return;
+            renderer.EnqueuePass(m_UsePixelationMapPass);
+        }
+
+        protected override void Dispose(bool disposing) {
+            base.Dispose(disposing);
+
+            if (disposing) {
+                m_PixelationMapPass.Dispose();
+            }
         }
 
         [Serializable]
         public class Settings {
-            public ComputeShader computeShader;
+            public ComputeShader drawPixelationMapShader;
+            public ComputeShader usePixelationShader;
             public RenderPassEvent renderPassEvent = RenderPassEvent.AfterRenderingOpaques;
             public bool previewInSceneView = true;
         }
--- ObjectSpacePixelation/UsePixelationMapPass.cs
+++ ObjectSpacePixelation/UsePixelationMapPass.cs
@@ -4,24 +4,24 @@ using UnityEngine.Rendering;
 using UnityEngine.Rendering.Universal;
 
 namespace XihePostProcessing.ObjectSpacePixelation {
-    public class ObjectSpacePixelationPass : ScriptableRenderPass {
+    public class UsePixelationMapPass : ScriptableRenderPass {
         private ObjectSpacePixelationFeature.Settings m_Settings;
         private ShaderTagId m_ObjectSpacePixelationShaderTag = new ShaderTagId("ObjectSpacePixelation");
 
         private RenderTexture m_SourceColorRT;
         private RenderTexture m_SourceDepthRT;
 
-        private RenderTargetHandle m_OutputRT;
+        private RTHandle m_OutputRT;
 
-        public ObjectSpacePixelationPass(ObjectSpacePixelationFeature.Settings settings) {
+        public UsePixelationMapPass(ObjectSpacePixelationFeature.Settings settings) {
             m_Settings = settings;
-            m_OutputRT.Init("_OutputRT");
+            m_OutputRT = RTHandles.Alloc("_OutputRT", name: "_OutputRT");
         }
 
         public override void Configure(CommandBuffer cmd, RenderTextureDescriptor cameraTextureDescriptor) {
             var descriptor = cameraTextureDescriptor;
             descriptor.enableRandomWrite = true;
-            cmd.GetTemporaryRT(m_OutputRT.id, descriptor);
+            cmd.GetTemporaryRT(Shader.PropertyToID(m_OutputRT.name), descriptor);
 
             m_SourceColorRT = RenderTexture.GetTemporary(cameraTextureDescriptor.width, cameraTextureDescriptor.height, 0, RenderTextureFormat.ARGB32);
             m_SourceDepthRT = RenderTexture.GetTemporary(cameraTextureDescriptor.width, cameraTextureDescriptor.height, 32, RenderTextureFormat.Depth);
@@ -43,21 +43,23 @@ namespace XihePostProcessing.ObjectSpacePixelation {
             cmd.Clear();
             context.DrawRenderers(renderingData.cullResults, ref drawDitherSetting, ref drawDitherFilter);
 
-            var shader = m_Settings.computeShader;
+            var shader = m_Settings.usePixelationShader;
             var mainKernel = shader.FindKernel("FillDitherTexture");
 
             //set parameters
-            cmd.SetComputeTextureParam(shader, mainKernel, "_CameraOpaqueTexture", renderingData.cameraData.renderer.cameraColorTarget);
-            cmd.SetComputeTextureParam(shader, mainKernel, "_CameraDepthTexture", renderingData.cameraData.renderer.cameraDepthTarget);
+            cmd.SetComputeTextureParam(shader, mainKernel, "_CameraOpaqueTexture", renderingData.cameraData.renderer.cameraColorTargetHandle);
+            cmd.SetComputeTextureParam(shader, mainKernel, "_CameraDepthTexture", renderingData.cameraData.renderer.cameraDepthTargetHandle);
             cmd.SetComputeTextureParam(shader, mainKernel, "_ObjectColorTexture", m_SourceColorRT);
             cmd.SetComputeTextureParam(shader, mainKernel, "_ObjectDepthTexture", m_SourceDepthRT);
-            cmd.SetComputeTextureParam(shader, mainKernel, "_OutputTexture", m_OutputRT.Identifier());
+            cmd.SetComputeFloatParam(shader, "_CameraNear", renderingData.cameraData.camera.nearClipPlane);
+            cmd.SetComputeFloatParam(shader, "_CameraFar", renderingData.cameraData.camera.farClipPlane);
+            cmd.SetComputeTextureParam(shader, mainKernel, "_OutputTexture", m_OutputRT);
 
             int threadGroupX = Mathf.CeilToInt(renderingData.cameraData.camera.scaledPixelWidth / 8.0f);
             int threadGroupY = Mathf.CeilToInt(renderingData.cameraData.camera.scaledPixelHeight / 8.0f);
             cmd.DispatchCompute(shader, mainKernel, threadGroupX, threadGroupY, 1);
 
-            cmd.Blit(m_OutputRT.id, renderingData.cameraData.renderer.cameraColorTarget);
+            cmd.Blit(m_OutputRT.nameID, renderingData.cameraData.renderer.cameraColorTargetHandle);
 
             context.ExecuteCommandBuffer(cmd);
             CommandBufferPool.Release(cmd);
@@ -67,6 +69,7 @@ namespace XihePostProcessing.ObjectSpacePixelation {
         public override void FrameCleanup(CommandBuffer cmd) {
             RenderTexture.ReleaseTemporary(m_SourceColorRT);
             RenderTexture.ReleaseTemporary(m_SourceDepthRT);
+            cmd.ReleaseTemporaryRT(Shader.PropertyToID(m_OutputRT.name));
         }
     }
 }
